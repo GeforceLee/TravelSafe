2ba8e2e88b02085371ef1cf85bf67f9000093861
GeforceLee <liyunlong@LiYunlongdeMacBook-Pro.local>
2016-06-02 21:05:29 +0800

    init

---
+++ TravelSafe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelSafe
+//
+//  Created by LiYunlong on 16/6/2.
+//  Copyright © 2016年 ezutrip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravelSafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravelSafe
+//
+//  Created by LiYunlong on 16/6/2.
+//  Copyright © 2016年 ezutrip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
